21ee4ae0226d0e693fdff1efd5e6ed7e65d97364
Or Cherfas <ocherfas@gmail.com>
2021-09-13 14:23:31 +0300

    remove install script

--- hack/install.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-
-mkdir -p /opt/hworker/bin
-cp $SCRIPTPATH/../out/hworker /opt/hworker/bin/
-cp -n $SCRIPTPATH/../config.yaml /opt/hworker/config.yaml
-ln -s /opt/hworker/bin/hworker /usr/bin/hworker
